aa53ebd533b21145958d4a249d928529f17bdfec
EL MEHDI EL GAROUAZ <eelgaro@MacBook-Pro-de-EL.local>
2026-08-19 18:30:47 +0100

    Inception first commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+
+COMPOSE = docker compose -f srcs/docker-compose.yml
+
+# --build: rebuild the images every time we change Dockerfile insted of building the container fron an existing image
+up:
+	$(COMPOSE) up --build -d
+
+down:
+	$(COMPOSE) down
+
+# docker system prune -f: removes stopped containers, unused networks, dangling (untagged) images, and build cache across all of Docker, not just this project. -f = force.
+clean: down
+	docker system prune -f
+
+
+fclean: down
+	docker system prune -af --volumes
+
+re: fclean up
+
+.PHONY: up down clean fclean re
---
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+DB_DATA_DIR=/var/lib/mysql
+
+# is the list of files in the data directory empty? ➜ 
+if [ -z "$(ls -A "$DB_DATA_DIR" 2>/dev/null)" ]; then
+	echo "No existing database found — initializing..."
+	mariadb-install-db --user=mysql --datadir="$DB_DATA_DIR" >/dev/null
+	# reading the passwords from Docker secrets
+	DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+	DB_PASSWORD=$(cat /run/secrets/db_password)
+
+	mysqld --user=mysql --bootstrap <<-EOSQL
+    USE mysql;
+    FLUSH PRIVILEGES;
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+    FLUSH PRIVILEGES;
+	EOSQL
+
+	echo "Initialization complete."
+fi
+
+exec mysqld --user=mysql
